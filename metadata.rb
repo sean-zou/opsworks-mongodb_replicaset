7df69bf64b2d96bc8e4b66f0da215c0e4a327068
Sean Zou <sean.zou@scanbuy.com>
2014-07-31 16:32:41 -0400

    Mongo Replicaset initial setup

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb_replicaset"
+description 'Example cookbook for MongoDB replicaset Berkshelf based install'
+maintainer  "Sean Zou"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
