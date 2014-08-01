5207acf230debdbd68d490b79003b4db04cb1991
Sean Zou <sean.zou@scanbuy.com>
2014-07-31 21:38:44 -0400

    Use mongodb_org_repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
-include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::default"
 include_recipe "mongodb::replicaset"
 include_recipe "mongodb::mms-agent"
